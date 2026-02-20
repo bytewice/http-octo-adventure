c4d6704da9647959499e0be7ecaeebaddb74ce78
bytewice <belc@cin.ufpe.br>
2026-02-20 18:59:58 -0300

    feature sesstokens

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,29 +1,65 @@
 // controllers/authController.js
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto'); // usando crypto so por enquanto q n coloquei o salt no database
+const cookieParser = require('cookie-parser');
 
-function authenticate(req, res) {
+const {checkValidSession, createUserSession, userExists, password_from_user} = require('../db/database');
+
+async function userHome(req, res) {
+
+    const token = req.cookies.sessionID;
+    
+    const tokendb = await checkValidSession(token);
+    console.log("Token recebido:", tokendb);
+    
+    //BRUNAO if tokens exists and its valid in database then sends /home
+    if(tokendb){
+        res.set("Content-Type", "text/plain; charset=utf-8");
+        return res.send(`✅ Login bem-sucedido! Bem-vindo ao /home)`);
+    }
+    // if atacker tries to access without a non-existent token, it will be redirected to /login
+    else{
+        res.status(401);
+        res.set("Content-Type", "text/plain; charset=utf-8");
+
+        return res.send("Deixa de invenção amigão :)");  
+    }
+}
+
+
+async function authenticate(req, res) {
     const { usuario, senha } = req.body;
 
-    //Exemplo hardcoded enquanto ta sem database
-    if (usuario === "brunao"){
+    const existe = await userExists(usuario);
+    
+    if (existe){
         const hash = crypto.createHash('sha256').update(senha).digest('hex');
-        if( hash === "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3" ){ // hash de "123"
-            const senhaCriptografada = hash;
-            res.set("Content-Type", "text/plain; charset=utf-8");
-            return res.send(`✅ Login bem-sucedido! Bem-vindo ao /home.`);
+        const password = await password_from_user(usuario);
+        if( hash === password ){
+                // Criar sessão e enviar *novo* cookie -> evitar session fixation
+                const token = crypto.randomBytes(32).toString('hex');
+                // VULN armazenando token hardcoded no banco
+                // tem q hashear o token antes de salvar no banco pra evitar sess. hijacking
+                await createUserSession(usuario, token); // Salva a sessão no banco de dados
+                res.cookie('sessionID', token, {
+                  httpOnly: true,  
+                  secure: false,    // o proj vai ser em http msm <---- vulnerável
+                  sameSite: 'strict', 
+                  maxAge: 3600000  // Tempo de vida do cookie (em milissegundos)
+                });
+            return res.redirect('/home');
         }
     }
-    else{ //tem q ter else pra nao ter enumeração de usuário
+    else{ //tem q ter else pra evitar ->> user enumeration
         const dumb_hash = crypto.createHash('sha256').update("dumb_hash").digest('hex');
     }
 
     // Caso credenciais erradas
     res.status(401);
     res.set("Content-Type", "text/plain; charset=utf-8");
-    return res.send("❌ Usuário ou senha incorretos");
+    return res.redirect('/login');
 }
 
 module.exports = {
-    authenticate
+    authenticate, userHome
 };
